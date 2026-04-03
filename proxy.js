a93d3c637cc6e7790d986978fcfe6cf9f5705c13
kentarou <kabuki@colla-tech.co.jp>
2026-04-03 13:57:55 +0900

    fix: rename middleware to proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { jwtVerify } from "jose"
 
-export async function middleware(request){
+export async function proxy(request){
     const token = await request.headers.get("Authorization")?.split(" ")[1]
 
     if(!token){
